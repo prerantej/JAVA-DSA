b907abfd654defdc81dce441522e9222da6a223c
prerantej <prerantejkvr@gmail.com>
2024-04-26 14:13:41 +0530

    temperature.java is learnt

---
+++ java_basics/temperature.java
@@ -0,0 +1,14 @@
+package java_basics;
+import java.util.Scanner;
+
+public class temperature {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature in Celsius");
+        float ctemp= sc.nextFloat();
+
+        float ftemp = (float) ((ctemp * 1.8) + 32);
+        System.out.println("Farenheit is " + ftemp);
+    }
+
+}
