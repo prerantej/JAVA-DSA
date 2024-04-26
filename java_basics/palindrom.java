3cbfa57b6e9dda9ddacffb6a6e4b8259c4ca04af
prerantej <prerantejkvr@gmail.com>
2024-04-26 23:03:59 +0530

    palindrom

---
+++ java_basics/palindrom.java
@@ -0,0 +1,21 @@
+package java_basics;
+import java.util.Scanner;
+
+public class palindrom {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String s = sc.nextLine();
+        String palindrom = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            palindrom = palindrom + s.charAt(i);
+    }
+        if (palindrom.equals(s)) {
+            System.out.println("yep a palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }
+
+    }
+}
