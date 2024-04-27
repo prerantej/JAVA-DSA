dbfe7eee786f3ff4c9344f474c98625b692e74d7
prerantej <prerantejkvr@gmail.com>
2024-04-27 16:39:07 +0530

    largest number code

---
+++ java_basics/largestNum.java
@@ -0,0 +1,35 @@
+package java_basics;
+import java.util.Scanner;
+
+public class largestNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 3 numbers:");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // find the largest number out of these method 1//
+
+       /*if(a>b && a>c){
+            System.out.println(a + " is larger than " + b + " and " + c);
+        }
+        else if(b>a && b>c){
+            System.out.println(b + " is larger than " + a + " and " + c);
+        }
+        else if(c>a && c>b){
+            System.out.println(c + " is larger than " + a + " and " + b);
+        }*/
+
+        //method 2//
+        int max= a;
+        if(b>a){
+            max=b;
+        }
+        else if(c>b){
+            max=c;
+        }
+        System.out.println(max);
+    }
+}
