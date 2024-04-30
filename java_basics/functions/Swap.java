0c9a85b8a0ae77ab337357b94b01e6900b10afe9
prerantej <prerantejkvr@gmail.com>
2024-04-30 17:15:56 +0530

    swap function

---
+++ java_basics/functions/Swap.java
@@ -0,0 +1,16 @@
+package java_basics.functions;
+
+public class Swap {
+    public static void main(String[] args) {
+        swap(2,3);
+    }
+    static void swap(int a, int b) {
+        System.out.println("a is equal to " + a);
+        System.out.println("b is equal to " + b);
+        int temp= a;
+        a=b;
+        b=temp;
+        System.out.println("now a is equal to " + a);
+        System.out.println("and b is equal to" + b);
+    }
+}
