2ef7fd888069c416c4c2b0dd2f80135d71adc6f4
prerantej <prerantejkvr@gmail.com>
2024-06-08 23:16:09 +0530

    bst

---
+++ java_basics/BST/Node.java
@@ -0,0 +1,13 @@
+package java_basics.BST;
+
+public class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        key= data;
+        left = null;
+        right = null;
+    }
+}
