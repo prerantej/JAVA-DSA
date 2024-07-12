cb2efa6ad833940e59e0a5145ed53bb2c3e0fa40
prerantej <prerantejkvr@gmail.com>
2024-07-12 20:11:59 +0530

    2 q

---
+++ java_basics/Arrays/SecondLargest.java
@@ -0,0 +1,28 @@
+package java_basics.Arrays;
+
+class SecondLargest {
+    int print2largest(int arr[], int n) {
+        int large = arr[0];
+        int secondLarge = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > large) {
+                secondLarge = large;
+                large = arr[i];
+            } else if (arr[i] > secondLarge && arr[i] != large) {
+                secondLarge = arr[i];
+            }
+        }
+        return secondLarge;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        int n = arr.length;
+
+        SecondLargest sl = new SecondLargest();
+        int result = sl.print2largest(arr, n);
+
+        System.out.println("The second largest element is: " + result);
+    }
+}
