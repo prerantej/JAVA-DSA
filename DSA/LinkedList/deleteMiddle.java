e02f7889fdfd8074271af282dc9b50028cf541ee
prerantej <prerantejkvr@gmail.com>
2024-09-08 20:47:43 +0530

    deleteMiddle

---
+++ DSA/LinkedList/deleteMiddle.java
@@ -0,0 +1,20 @@
+package DSA.LinkedList;
+
+class deleteMiddle {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode prev=null;
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
