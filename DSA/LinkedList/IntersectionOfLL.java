cab2b819df0ee0b909719a60117c419ce78c02e3
prerantej <prerantejkvr@gmail.com>
2024-09-09 01:30:34 +0530

    Intersection of linkedlist by using 2 pointers

---
+++ DSA/LinkedList/IntersectionOfLL.java
@@ -0,0 +1,14 @@
+package DSA.LinkedList;
+
+public class IntersectionOfLL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
