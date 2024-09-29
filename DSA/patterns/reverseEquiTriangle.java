32314734987e0f7ea32180779fb334762ee92cdb
prerantej <prerantejkvr@gmail.com>
2024-09-29 23:39:44 +0530

    pattern

---
+++ DSA/patterns/reverseEquiTriangle.java
@@ -0,0 +1,28 @@
+package DSA.patterns;
+
+import java.util.Scanner;
+
+public class reverseEquiTriangle {
+    public static void triangle(int n){
+        for(int i =0; i<n;i++){
+            for (int j = i; j >0 ; j--) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2*(n-i-1)+1; j++){
+                System.out.print("*");
+            }
+            for (int j = i; j >0 ; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        triangle(x);
+    }
+}
