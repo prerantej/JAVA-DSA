54210f006a10a65b23616d5a3b6167084c92e3c1
prerantej <prerantejkvr@gmail.com>
2024-11-03 20:35:47 +0530

    Rotate String Input: s = "abcde", goal = "cdeab" Output: true

---
+++ DSA/Leetcodedaily/RotateString.java
@@ -0,0 +1,10 @@
+package DSA.Leetcodedaily;
+
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
