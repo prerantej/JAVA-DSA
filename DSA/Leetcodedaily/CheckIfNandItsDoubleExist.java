0bac4fc6990ee2f15701fcd4f2f8c1242a4ff747
prerantej <prerantejkvr@gmail.com>
2024-12-01 22:07:07 +0530

    1346. Check If N and Its Double Exist

---
+++ DSA/Leetcodedaily/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,18 @@
+package DSA.Leetcodedaily;
+import java.util.HashSet;
+
+public class CheckIfNandItsDoubleExist {
+
+    public class Solution {
+        public boolean checkIfExist(int[] arr) {
+            HashSet<Integer> set = new HashSet<>();
+            for (int num : arr) {
+                if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                    return true;
+                }
+                set.add(num);
+            }
+            return false;
+        }
+    }
+}
